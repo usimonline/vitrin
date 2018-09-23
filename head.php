e82771643b691463b968713dbbbeb76e63709cc8
usimonline <usim.online@mail.ru>
2018-09-23 18:33:44 +0300

    1

---
+++ head.php
@@ -0,0 +1,64 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Акции | Студия Витрин - салон красоты в Минске</title>
+<meta property="og:url" content="http://<?php echo $site_name; ?>/">
+<meta property="og:title" content="Акции | Студия Витрин - салон красоты в Минске">
+<meta property="og:description" content="Акции | Имидж-студия Витрин Lab, г. Минск, м. Площадь Победы, пр. Машерова д.9 оф. 208">
+<meta property="og:type" content="website">
+<meta property="og:image" content="https://<?php echo $site_name; ?>/index/44444.jpg">
+<meta property="fb:app_id" content="257953674358265">
+<meta name="format-detection" content="telephone=no">
+<meta http-equiv="x-dns-prefetch-control" content="on">
+<meta name="description" content="Акции | Имидж-студия Витрин Lab, г. Минск, м. Площадь Победы, пр. Машерова д.9 оф. 208">
+<meta name="keywords" content="элитный салон красоты, элитные салоны красоты +в Минске, центр красоты Минск, центр красоты, услуги студии красоты, услуги салона красоты Минск, услуги салона красоты, телефон салона красоты, студия красоты цены, студия красоты Площадь Победы, студия красоты Якуба Коласа">
+<link rel="canonical" href="http://<?php echo $site_name; ?>/">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<!-- Assets -->
+<link rel="stylesheet" href="./index/tilda-grid-3.0.min.css" type="text/css" media="all">
+<link rel="stylesheet" href="./index/tilda-blocks-2.12.css" type="text/css" media="all">
+<link rel="stylesheet" href="./index/tilda-animation-1.0.min.css" type="text/css" media="all">
+<link rel="stylesheet" href="./index/tilda-slds-1.4.min.css" type="text/css" media="all">
+<link rel="stylesheet" href="./index/tilda-zoom-2.0.min.css" type="text/css" media="all">
+<link rel="stylesheet" href="./index/tilda-popup-1.1.min.css" type="text/css" media="all">
+
+
+<script type="text/javascript" async="" id="tildastatscript" src="./index/tildastat-0.2.min.js.Без названия"></script>
+<script async="" src="./index/analytics.js.Без названия"></script>
+<script type="text/javascript" async="" src="./index/watch.js.Без названия"></script>
+<script async="" src="./index/gtm.js.Без названия"></script>
+<script async="" src="./index/fbevents.js.Без названия"></script>
+<script src="./index/jquery-1.10.2.min.js.Без названия"></script>
+<script src="./index/tilda-scripts-2.8.min.js.Без названия"></script>
+<script src="./index/tilda-blocks-2.7.js.Без названия"></script>
+<script src="./index/lazyload-1.3.min.js.Без названия"></script>
+<script src="./index/tilda-animation-1.0.min.js.Без названия"></script>
+<script src="./index/tilda-slds-1.4.min.js.Без названия"></script>
+<script src="./index/hammer.min.js.Без названия"></script>
+<script src="./index/tilda-forms-1.0.min.js.Без названия"></script>
+<script src="./index/tilda-map-1.0.min.js.Без названия"></script>
+
+
+<!-- Facebook Pixel Code -->
+
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '197189404161315');
+  fbq('track', 'PageView');
+</script>
+
+<noscript>
+  <img height="1" width="1" style="display:none"
+       src="https://www.facebook.com/tr?id=197189404161315&ev=PageView&noscript=1"
+  /></noscript>
+
+
+<!-- End Facebook Pixel Code -->
+<script type="text/javascript">window.dataLayer = window.dataLayer || [];</script>
+

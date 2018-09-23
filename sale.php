c4da9cefc3009e518307f55d87849af201e654a4
usimonline <usim.online@mail.ru>
2018-09-23 18:39:31 +0300

    1

--- sale.php
+++ sale.php
@@ -65,93 +65,10 @@ require_once ('requisites.php');
   </div>
 
 
-  
-  
 
-  <!--footer-->
-
-  <div id="t-footer" class="t-records t-records_animated t-records_visible" data-hook="blocks-collection-content-node" data-tilda-project-id="538753" data-tilda-page-id="3191027" data-tilda-formskey="3f508fc2ea7d23fde77f4bd2720b21b7">
-    <div id="rec60130970" class="r t-rec t-rec_pt_30 t-rec_pb_30" style="padding-top:30px;padding-bottom:30px;background-color:#ffffff; " data-animationappear="off" data-record-type="433" data-bg-color="#ffffff"><!-- t433 -->
-      <div class="t433">
-        <div class="t-width t-width_100 t433__mainblock" style="margin:0 auto; background-color:#000000;">
-          <div class="t433__map t433__col" style="height: 500px;">
-
-            <iframe src="https://yandex.by/map-widget/v1/-/CBBRU6wTtA" width="600" scrolling="no" frameborder="0"
-                    style="height: 500px; overflow: hidden;  min-width: 124px !important; min-height: 34px !important;">
-
-            </iframe>
-
-          </div>
-
-          <div class="t433__col t433__col_text" style="background-color:#000000;">
-            <div class="t433__contentbox" style="background-color:#000000; height:500px;">
-              <div class="t433__contentwrapper">
-
-                <div class="t433__title t-descr t-descr_xxl t-animate t-animate_started" data-animate-style="fadeindown" data-animate-group="yes" data-animate-order="1" field="title" style="color: rgb(255, 255, 255); transition-delay: 0s;"><div style="font-size:28px;" data-customstyle="yes">КОНТАКТЫ</div></div> <div class="t-animate t-animate_started" data-animate-style="fadein" data-animate-group="yes" data-animate-order="5" data-animate-delay="0.3" style="transition-delay: 0.3s;"> <div class="t-sociallinks"> <div class="t-sociallinks__wrapper"> <div class="t-sociallinks__item"> <a href="https://www.facebook.com/msmaria.khramtsova/" target="_blank"> <svg class="t-sociallinks__svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 48 48" enable-background="new 0 0 48 48" xml:space="preserve"><desc>Facebook</desc><path style="fill:#ffffff;" d="M47.761,24c0,13.121-10.638,23.76-23.758,23.76C10.877,47.76,0.239,37.121,0.239,24c0-13.124,10.638-23.76,23.764-23.76C37.123,0.24,47.761,10.876,47.761,24 M20.033,38.85H26.2V24.01h4.163l0.539-5.242H26.2v-3.083c0-1.156,0.769-1.427,1.308-1.427h3.318V9.168L26.258,9.15c-5.072,0-6.225,3.796-6.225,6.224v3.394H17.1v5.242h2.933V38.85z"></path></svg> </a> </div> <div class="t-sociallinks__item"> <a href="https://www.youtube.com/channel/UCS58pOeniij3BIcwppToNDA/" target="_blank"> <svg class="t-sociallinks__svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="-455 257 48 48" enable-background="new -455 257 48 48" xml:space="preserve"><desc>Youtube</desc><path style="fill:#ffffff;" d="M-431,257.013c13.248,0,23.987,10.74,23.987,23.987s-10.74,23.987-23.987,23.987s-23.987-10.74-23.987-23.987S-444.248,257.013-431,257.013z M-419.185,275.093c-0.25-1.337-1.363-2.335-2.642-2.458c-3.054-0.196-6.119-0.355-9.178-0.357c-3.059-0.002-6.113,0.154-9.167,0.347c-1.284,0.124-2.397,1.117-2.646,2.459c-0.284,1.933-0.426,3.885-0.426,5.836s0.142,3.903,0.426,5.836c0.249,1.342,1.362,2.454,2.646,2.577c3.055,0.193,6.107,0.39,9.167,0.39c3.058,0,6.126-0.172,9.178-0.37c1.279-0.124,2.392-1.269,2.642-2.606c0.286-1.93,0.429-3.879,0.429-5.828C-418.756,278.971-418.899,277.023-419.185,275.093zM-433.776,284.435v-7.115l6.627,3.558L-433.776,284.435z"></path></svg> </a> </div> <div class="t-sociallinks__item"> <a href="https://www.instagram.com/ms.mariakhramtsova/" target="_blank"> <svg class="t-sociallinks__svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" height="30px" viewBox="0 0 30 30" xml:space="preserve"><desc>Instagram</desc><path style="fill:#ffffff;" d="M15,11.014 C12.801,11.014 11.015,12.797 11.015,15 C11.015,17.202 12.802,18.987 15,18.987 C17.199,18.987 18.987,17.202 18.987,15 C18.987,12.797 17.199,11.014 15,11.014 L15,11.014 Z M15,17.606 C13.556,17.606 12.393,16.439 12.393,15 C12.393,13.561 13.556,12.394 15,12.394 C16.429,12.394 17.607,13.561 17.607,15 C17.607,16.439 16.444,17.606 15,17.606 L15,17.606 Z"></path><path style="fill:#ffffff;" d="M19.385,9.556 C18.872,9.556 18.465,9.964 18.465,10.477 C18.465,10.989 18.872,11.396 19.385,11.396 C19.898,11.396 20.306,10.989 20.306,10.477 C20.306,9.964 19.897,9.556 19.385,9.556 L19.385,9.556 Z"></path><path style="fill:#ffffff;" d="M15.002,0.15 C6.798,0.15 0.149,6.797 0.149,15 C0.149,23.201 6.798,29.85 15.002,29.85 C23.201,29.85 29.852,23.202 29.852,15 C29.852,6.797 23.201,0.15 15.002,0.15 L15.002,0.15 Z M22.666,18.265 C22.666,20.688 20.687,22.666 18.25,22.666 L11.75,22.666 C9.312,22.666 7.333,20.687 7.333,18.28 L7.333,11.734 C7.333,9.312 9.311,7.334 11.75,7.334 L18.25,7.334 C20.688,7.334 22.666,9.312 22.666,11.734 L22.666,18.265 L22.666,18.265 Z"></path></svg> </a> </div> </div></div> </div>
-
-                <div class="t433__text t-descr t-descr_xxs t-animate t-animate_started" data-animate-style="fadeinleft" data-animate-group="yes" data-animate-order="6" data-animate-delay="0.3" field="text" style="color: rgb(255, 255, 255); transition-delay: 0.6s;">
-
-                  <div style="font-size:18px;color:#ffffff;" data-customstyle="yes">
-                    <a href="tel:+375296340205" style="color:#ffffff !important;">+375 29 634 02 05
-                    </a>
-                    <br>
-                    <a href="https://yandex.by/maps/-/CBBNbTAppB" style="color:#ffffff !important;"><br>м. Площадь Победы<br>пр. Машерова, д.9 оф. 208
-                    </a>
-                    <br>
-
-                  </div>
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-
-    </div>
-    <div id="rec64313855" class="r t-rec" style=" " data-animationappear="off" data-record-type="131"><!-- T123 -->
-      <div class="t123">
-        <div class="t-container_100 "><div class="t-width t-width_100 ">
-
-            <style> .son_wrapper { width: 150px; height: 150px; position: fixed; bottom: 0;right: 0; border:0px solid #f00; z-index:9999; } .son_wrapper a { text-decoration:none; } .son_circle { width:100px; height:100px; border-radius:250px; font-size:14px; font-family: Play, sans-serif !important; color:#fff; line-height:25px; text-align:center; background:#f00; } @keyframes son_outercircle { 0% { transform: scale(1,1) opacity: 1; } 80% { opacity: 0.8; } 100% { transform: scale(2,2); opacity: 0; } } .son_outercircle { z-index: -1000; position: absolute; width: 60px; height: 60px; border: 5px solid #f00; border-radius: 50%; animation: son_outercircle 2s infinite linear; margin-top: 15px ; margin-left: 15px ; }
-            </style>
-
-            <div class="son_wrapper">
-
-              <div class="son_outercircle"></div>
-
-              <a href="http://<?php echo $site_name; ?>/?utm_source=google&amp;utm_medium=cpc&amp;utm_term=%D0%B8%D0%BC%D0%B8%D0%B4%D0%B6%20%2B%D1%81%D1%82%D1%83%D0%B4%D0%B8%D1%8F%20%2B%D0%BC%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&amp;utm_campaign={campaign_id}#" onclick="showSonlineWidget(sonlineWidgetOptions);">
-                <div class="son_circle"><br>Онлайн<br> запись
-                </div>
-              </a>
-            </div>
-
-            <script>var sonlineWidgetOptions = {placeid: 60}</script>
-            <div id="widgetSonline">
-              <script type="text/javascript" src="./index/widget3.js.Без названия"></script>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!--/footer-->
-
-  </div>
-
-
-  <!--/allrecords--><!-- Tilda copyright. Don't remove this line -->
-  <div class="t-tildalabel " id="tildacopy" data-tilda-sign="538753#3190780"><a href="https://tilda.cc/?upm=538753" class="t-tildalabel__link"><div class="t-tildalabel__wrapper"><div class="t-tildalabel__txtleft">Made on </div><div class="t-tildalabel__wrapimg"><img src="./index/tildacopy.png" class="t-tildalabel__img"></div><div class="t-tildalabel__txtright">Tilda</div></div></a>
-  </div><!-- Stat -->
-
-
-
-
-
-  <script type="text/javascript">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');	ga('create', 'UA-116495850-5', 'auto');	ga('send', 'pageview');	window.mainTracker = 'user';	if (! window.mainTracker) { window.mainTracker = 'tilda'; }	(function (d, w, k, o, g) { var n=d.getElementsByTagName(o)[0],s=d.createElement(o),f=function(){n.parentNode.insertBefore(s,n);}; s.type = "text/javascript"; s.async = true; s.key = k; s.id = "tildastatscript"; s.src=g; if (w.opera=="[object Opera]") {d.addEventListener("DOMContentLoaded", f, false);} else { f(); } })(document, window, 'cc0eb2ee77c0b749f57ff58c75f3cfae','script','https://stat.tildacdn.com/js/tildastat-0.2.min.js');
-  </script>
+  <?php
+  require_once ('footer.php');
+  ?>
 
 </div>
 

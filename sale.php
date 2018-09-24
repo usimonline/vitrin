282fc03a1e7bfdc8c0eeef37dcdd022c25ec87f2
usimonline <usim.online@mail.ru>
2018-09-24 18:15:46 +0300

    1

--- sale.php
+++ sale.php
@@ -30,8 +30,10 @@ require_once ('requisites.php');
   <div id="rec60127453" class="r t-rec t-rec_pt_30 t-rec_pb_15" style="padding-top:30px;padding-bottom:15px; " data-animationappear="off" data-record-type="608"><!-- T608 -->
     <div class="t608">
       <div class="t-container">
+
+
         <div class="t608__col t-col t-col_4">
-          <div class="t608__bgimg t-margin_auto t-bgimg loaded" data-image-width="360" data-image-height="240" bgimgfield="img" data-original="https://static.tildacdn.com/tild3462-3639-4531-a362-333862666664/image.png" style="background-image: url(&quot;https://static.tildacdn.com/tild3462-3639-4531-a362-333862666664/image.png&quot;); padding-bottom: 66.6667%;" src="">
+          <div class="t608__bgimg t-margin_auto t-bgimg loaded" data-image-width="360" data-image-height="240" bgimgfield="img" data-original="./index/sale_001.jpg" style="background-image: url(&quot;./index/sale_001.jpg&quot;); padding-bottom: 66.6667%;" src="">
 
           </div>
           <div class="t608__textwrapper t-align_center">
@@ -45,8 +47,9 @@ require_once ('requisites.php');
             </a>
           </div>
         </div>
+
         <div class="t608__col t-col t-col_4">
-          <div class="t608__bgimg t-margin_auto t-bgimg loaded" data-image-width="360" data-image-height="240" bgimgfield="img2" data-original="https://static.tildacdn.com/tild6562-6137-4536-b535-663863396564/0001__.jpg" style="background-image: url(&quot;https://static.tildacdn.com/tild6562-6137-4536-b535-663863396564/0001__.jpg&quot;); padding-bottom: 66.6667%;" src="">
+          <div class="t608__bgimg t-margin_auto t-bgimg loaded" data-image-width="360" data-image-height="240" bgimgfield="img2" data-original="./index/sale_002.jpg" style="background-image: url(&quot;./index/sale_002.jpg&quot;); padding-bottom: 66.6667%;" src="">
 
           </div>
           <div class="t608__textwrapper t-align_center">
@@ -58,7 +61,23 @@ require_once ('requisites.php');
             </a>
           </div>
         </div>
+
+        <div class="t608__col t-col t-col_4">
+          <div class="t608__bgimg t-margin_auto t-bgimg loaded" data-image-width="360" data-image-height="240" bgimgfield="img2" data-original="./index/sale_003.jpg" style="background-image: url(&quot;./index/sale_003.jpg&quot;); padding-bottom: 66.6667%;" src="">
+
+          </div>
+          <div class="t608__textwrapper t-align_center">
+            <div class="t608__title t-heading t-heading_xs" style="" field="title2">Клиентский день!
+            </div>
+            <div class="t608__descr t-descr t-descr_xs" style="" field="descr2">-20% на все услуги красоты и профессиональную косметику, а также фуршет и атмосфера праздника!
+            </div>
+            <a href="http://persona-ostozhenka.ru/sale#Kday" target="" class="t608__btn t-btn t-btn_sm" style="color:#222222;border:1px solid #222222;border-radius:3px; -moz-border-radius:3px; -webkit-border-radius:3px;"><table style="width:100%; height:100%;"><tbody><tr><td>Получить предложение!</td></tr></tbody></table>
+            </a>
+          </div>
+        </div>
+
       </div>
+
     </div>
     <script type="text/javascript"> $(document).ready(function() { t608_setHeight('60127453'); });
     </script>

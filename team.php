1effed2a5fcaf0f216fce674288853ac6c9c6870
usimonline <usim.online@mail.ru>
2018-09-24 18:49:26 +0300

    1

--- team.php
+++ team.php
@@ -33,21 +33,102 @@ require_once ('requisites.php');
 
 
 
-  <div id="rec60130109" class="r t-rec t-rec_pt_0 t-rec_pb_0" style="padding-top:0px;padding-bottom:0px; " data-record-type="545"><!-- T545 --><div style="width: 0; height: 0; overflow: hidden;"> <div class="t545__sizer t-col t-col_6" data-auto-correct-mobile-width="false" style="height:70vh;"></div></div><div class="t545"> <div class="t-container" data-height="70"> <div class="t545__top t545__col t-col t-col_6 "> <div class="t545__blockimg t-bgimg loaded" bgimgfield="img" data-original="https://static.tildacdn.com/tild3034-3438-4263-a435-313765313235/image.png" style="background-image: url(&quot;https://static.tildacdn.com/tild3034-3438-4263-a435-313765313235/image.png&quot;); height: 438px;" src=""></div> </div> <div class="t-col t545__col t-col_6 "> <div class="t545__textwrapper t-align_left" style="background-color: rgb(255, 255, 255); height: 438px;"> <div class="t545__content t-valign_middle"> <div class="t545__box"><div class="t545__title t-heading t-heading_xs " field="title" style="">Александр Кобзев</div> <div class="t545__descr t-descr t-descr_xs " field="descr" style="">Арт-директор</div> <div class="t545__line " style="background-color: #ffffff;"></div> <div class="t545__text t-text t-text_sm " field="text" style="">Стаж работы 23 года. Прошёл обучение в ведущих школах: "ПЕРСОНА", WEIIA, TONI &amp; GUY, ALDO COPPOLA. Широкий спектр услуг: Стрижки, женские, мужские, детские. Коррекция бороды и усов. Является экспертным колористом. Выполнение всех видов окрашивания.</div> </div> </div> </div> </div> </div></div><script type="text/javascript"> $(window).resize(function() {	var t545_doResize = $('#rec60130109').data('resize-timeout'); if (t545_doResize) {clearTimeout(t545_doResize)}; t545_doResize = setTimeout(function() { t545_setHeight('60130109'); }, 500); $('#rec60130109').data('resize-timeout', t545_doResize);	}); $(document).ready(function() { setTimeout(function(){ t545_setHeight('60130109'); }, 50); }); $('.t545').bind('displayChanged',function(){ t545_setHeight('60130109'); }); </script>
+  <div id="rec60130109" class="r t-rec t-rec_pt_0 t-rec_pb_0" style="padding-top:0px;padding-bottom:0px; " data-record-type="545">
+    <!-- T545 --><div style="width: 0; height: 0; overflow: hidden;">
+      <div class="t545__sizer t-col t-col_6" data-auto-correct-mobile-width="false" style="height:70vh;">
+
+      </div></div><div class="t545">
+      <div class="t-container" data-height="70">
+        <div class="t545__top t545__col t-col t-col_6 ">
+          <div class="t545__blockimg t-bgimg loaded" bgimgfield="img" data-original="./index/rabotnik_03.png" style="background-image: url(&quot;./index/rabotnik_03.png&quot;); height: 438px;" src=""></div>
+        </div> <div class="t-col t545__col t-col_6 ">
+          <div class="t545__textwrapper t-align_left" style="background-color: rgb(255, 255, 255); height: 438px;">
+            <div class="t545__content t-valign_middle">
+              <div class="t545__box">
+                <div class="t545__title t-heading t-heading_xs " field="title" style="">Мария Храмцова
+                </div>
+                <div class="t545__descr t-descr t-descr_xs " field="descr" style="">Арт-директор
+                </div>
+                <div class="t545__line " style="background-color: #ffffff;">
+
+                </div>
+                <div class="t545__text t-text t-text_sm " field="text" style="">Стаж работы 23 года. Прошёл обучение в ведущих школах: "ПЕРСОНА", WEIIA, TONI &amp; GUY, ALDO COPPOLA. Широкий спектр услуг: Стрижки, женские, мужские, детские. Коррекция бороды и усов. Является экспертным колористом. Выполнение всех видов окрашивания.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script type="text/javascript"> $(window).resize(function() {	var t545_doResize = $('#rec60130109').data('resize-timeout'); if (t545_doResize) {clearTimeout(t545_doResize)}; t545_doResize = setTimeout(function() { t545_setHeight('60130109'); }, 500); $('#rec60130109').data('resize-timeout', t545_doResize);	}); $(document).ready(function() { setTimeout(function(){ t545_setHeight('60130109'); }, 50); }); $('.t545').bind('displayChanged',function(){ t545_setHeight('60130109'); });
+    </script>
+
 
   </div>
 
 
 
-  <div id="rec60127376" class="r t-rec t-rec_pt_45 t-rec_pb_150" style="padding-top:45px;padding-bottom:150px; " data-record-type="531"><!-- T531 --><div class="t531"><div class="t-container"> <div class="t531__row" style=""> <div class="t-col t-col_3 t531__leftcol"> <div class="t531__imgwrapper t531__imgwrapper_lg"> <div class="t531__blockimg t-bgimg loaded" bgimgfield="li_img__1478078229569" data-original="https://static.tildacdn.com/tild3766-6632-4330-b030-623636313233/image.png" style="background-image: url(&quot;https://static.tildacdn.com/tild3766-6632-4330-b030-623636313233/image.png&quot;); padding-bottom: 146.154%;" data-image-width="260" data-image-height="380" src=""></div> </div> </div> <div class="t-col t-col_5 t531__rightcol"> <div class="t531__textwrapper t-align_left" style="height: 380px;"> <div class="t531__content t-valign_middle"> <div class="t531__box"> <div class="t531__title t-name t-name_xl t531__bottommargin_sm" field="li_persname__1478078229569" style="">Ольга Бизина</div> <div class="t531__persdescr t-descr t-descr_xs t531__bottommargin_lg" field="li_persdescr__1478078229569" style="">Топ-стилист</div><div class="t531__text t-text t-text_sm " field="li_text__1478078229569" style="">Автор программы «Система ключей. Женские стрижки» и креативного курса #createcut</div> </div> </div> </div> </div> </div> <div class="t531__row" style=""> <div class="t-col t-col_3 t531__leftcol"> <div class="t531__imgwrapper t531__imgwrapper_lg"> <div class="t531__blockimg t-bgimg loaded" bgimgfield="li_img__1478078244983" data-original="https://static.tildacdn.com/tild6261-6634-4065-b962-316166666431/image.png" style="background-image: url(&quot;https://static.tildacdn.com/tild6261-6634-4065-b962-316166666431/image.png&quot;); padding-bottom: 146.154%;" data-image-width="260" data-image-height="380" src=""></div> </div> </div> <div class="t-col t-col_5 t531__rightcol"> <div class="t531__textwrapper t-align_left" style="height: 380px;"> <div class="t531__content t-valign_middle"> <div class="t531__box"> <div class="t531__title t-name t-name_xl t531__bottommargin_sm" field="li_persname__1478078244983" style="">Татьяна Стрельцова</div> <div class="t531__persdescr t-descr t-descr_xs t531__bottommargin_lg" field="li_persdescr__1478078244983" style="">Топ-стилист<br></div><div class="t531__text t-text t-text_sm " field="li_text__1478078244983" style="">Стилист-визажист. Училась в Академии Долорес, проходила повышение квалификации в ведущих школах парикмахерского искусства Vidal Sassoon, Tony and Guy. Индивидуальный подход к каждому клиенту.<br></div> </div> </div> </div> </div> </div> <div class="t531__row" style=""> <div class="t-col t-col_3 t531__leftcol"> <div class="t531__imgwrapper t531__imgwrapper_lg"> <div class="t531__blockimg t-bgimg loaded" bgimgfield="li_img__1478078768604" data-original="https://static.tildacdn.com/tild3532-3438-4264-b639-396361313938/image.png" style="background-image: url(&quot;https://static.tildacdn.com/tild3532-3438-4264-b639-396361313938/image.png&quot;); padding-bottom: 146.154%;" data-image-width="260" data-image-height="380" src=""></div> </div> </div> <div class="t-col t-col_5 t531__rightcol"> <div class="t531__textwrapper t-align_left" style="height: 380px;"> <div class="t531__content t-valign_middle"> <div class="t531__box"> <div class="t531__title t-name t-name_xl t531__bottommargin_sm" field="li_persname__1478078768604" style="">Алла Самботян</div> <div class="t531__persdescr t-descr t-descr_xs t531__bottommargin_lg" field="li_persdescr__1478078768604" style="">Визажист</div><div class="t531__text t-text t-text_sm " field="li_text__1478078768604" style="">Работает в Персоне более 15 лет. Мастер-универсал: стрижки мужские и женские, окрашивание, уход за волосами, а также визаж, коррекция и окрашивание бровей и ресниц.<br></div> </div> </div> </div> </div> </div> <div class="t531__row" style=""> <div class="t-col t-col_3 t531__leftcol"> <div class="t531__imgwrapper t531__imgwrapper_lg"> <div class="t531__blockimg t-bgimg loaded" bgimgfield="li_img__1531224047299" data-original="https://static.tildacdn.com/tild3634-3037-4536-b734-313430333739/image.png" style="background-image: url(&quot;https://static.tildacdn.com/tild3634-3037-4536-b734-313430333739/image.png&quot;); padding-bottom: 146.154%;" data-image-width="260" data-image-height="380" src=""></div> </div> </div> <div class="t-col t-col_5 t531__rightcol"> <div class="t531__textwrapper t-align_left" style="height: 380px;"> <div class="t531__content t-valign_middle"> <div class="t531__box"> <div class="t531__title t-name t-name_xl t531__bottommargin_sm" field="li_persname__1531224047299" style="">Оксана Щекатурова</div> <div class="t531__persdescr t-descr t-descr_xs t531__bottommargin_lg" field="li_persdescr__1531224047299" style="">Визажист</div><div class="t531__text t-text t-text_sm " field="li_text__1531224047299" style="">Опыт работы 17 лет, мастер по всем видам макияжа. Мастер по коррекции и окрашиванию бровей.<br></div> </div> </div> </div> </div> </div></div></div><script type="text/javascript"> var t531_doResize; $(window).resize(function(){ if (t531_doResize) clearTimeout(t531_doResize); t531_doResize = setTimeout(function() { t531_setHeight('60127376'); }, 200); }); $(document).ready(function() { t531_setHeight('60127376'); });</script>
+  <div id="rec60127376" class="r t-rec t-rec_pt_45 t-rec_pb_150" style="padding-top:45px;padding-bottom:150px; " data-record-type="531"><!-- T531 -->
+    <div class="t531">
+      <div class="t-container">
+        <div class="t531__row" style=""> <div class="t-col t-col_3 t531__leftcol">
+            <div class="t531__imgwrapper t531__imgwrapper_lg">
+              <div class="t531__blockimg t-bgimg loaded" bgimgfield="li_img__1478078229569" data-original="./index/rabotnik_02.png" style="background-image: url(&quot;./index/rabotnik_02.png&quot;); padding-bottom: 146.154%;" data-image-width="260" data-image-height="380" src="">
+
+              </div>
+            </div>
+          </div>
+          <div class="t-col t-col_5 t531__rightcol">
+            <div class="t531__textwrapper t-align_left" style="height: 380px;">
+              <div class="t531__content t-valign_middle">
+                <div class="t531__box">
+                  <div class="t531__title t-name t-name_xl t531__bottommargin_sm" field="li_persname__1478078229569" style="">Ольга Бизина
+                  </div>
+                  <div class="t531__persdescr t-descr t-descr_xs t531__bottommargin_lg" field="li_persdescr__1478078229569" style="">Топ-стилист
+                  </div>
+                  <div class="t531__text t-text t-text_sm " field="li_text__1478078229569" style="">Автор программы «Система ключей. Женские стрижки» и креативного курса #createcut
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="t531__row" style="">
+          <div class="t-col t-col_3 t531__leftcol">
+            <div class="t531__imgwrapper t531__imgwrapper_lg">
+              <div class="t531__blockimg t-bgimg loaded" bgimgfield="li_img__1478078244983" data-original="./index/rabotnik_01.png" style="background-image: url(&quot;./index/rabotnik_01.png&quot;); padding-bottom: 146.154%;" data-image-width="260" data-image-height="380" src="">
+
+              </div>
+            </div>
+          </div>
+          <div class="t-col t-col_5 t531__rightcol">
+            <div class="t531__textwrapper t-align_left" style="height: 380px;">
+              <div class="t531__content t-valign_middle">
+                <div class="t531__box">
+                  <div class="t531__title t-name t-name_xl t531__bottommargin_sm" field="li_persname__1478078244983" style="">Имиджмейкер</div>
+                  <div class="t531__persdescr t-descr t-descr_xs t531__bottommargin_lg" field="li_persdescr__1478078244983" style="">Топ-стилист<br></div><div class="t531__text t-text t-text_sm " field="li_text__1478078244983" style="">Стилист-визажист. Училась в Академии Долорес, проходила повышение квалификации в ведущих школах парикмахерского искусства Vidal Sassoon, Tony and Guy. Индивидуальный подход к каждому клиенту.<br>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+      </div>
+    </div>
+
+    <script type="text/javascript"> var t531_doResize; $(window).resize(function(){ if (t531_doResize) clearTimeout(t531_doResize); t531_doResize = setTimeout(function() { t531_setHeight('60127376'); }, 200); }); $(document).ready(function() { t531_setHeight('60127376'); });
+    </script>
   </div>
 
-  <div id="rec60127377" class="r t-rec t-rec_pt_0 t-rec_pb_150" style="padding-top:0px;padding-bottom:150px; " data-record-type="531"><!-- T531 --><div class="t531"><div class="t-container"> <div class="t531__row" style=""> <div class="t-col t-col_3 t531__leftcol"> <div class="t531__imgwrapper t531__imgwrapper_lg"> <div class="t531__blockimg t-bgimg loaded" bgimgfield="li_img__1478078229569" data-original="https://static.tildacdn.com/tild3662-3932-4633-b434-343665373738/image.png" style="background-image: url(&quot;https://static.tildacdn.com/tild3662-3932-4633-b434-343665373738/image.png&quot;); padding-bottom: 146.154%;" data-image-width="260" data-image-height="380" src=""></div> </div> </div> <div class="t-col t-col_5 t531__rightcol"> <div class="t531__textwrapper t-align_left" style="height: 380px;"> <div class="t531__content t-valign_middle"> <div class="t531__box"> <div class="t531__title t-name t-name_xl t531__bottommargin_sm" field="li_persname__1478078229569" style="">Вера Запорожцева</div> <div class="t531__persdescr t-descr t-descr_xs t531__bottommargin_lg" field="li_persdescr__1478078229569" style="">Nail-мастер</div><div class="t531__text t-text t-text_sm " field="li_text__1478078229569" style="">Владеет техниками классического и комбинированного маникюра, педикюра. Проходила обучение в ведущих nail фирмах такие как OPI, Luxio, ZUDA, Gehwol, CND<br><br></div> </div> </div> </div> </div> </div></div></div><script type="text/javascript"> var t531_doResize; $(window).resize(function(){ if (t531_doResize) clearTimeout(t531_doResize); t531_doResize = setTimeout(function() { t531_setHeight('60127377'); }, 200); }); $(document).ready(function() { t531_setHeight('60127377'); });</script>
-  </div>
 
 
 
 
+<!-- Это заявка на участие в команде-->
 
   <div id="rec60127381" class="r t-rec t-rec_pt_0 t-rec_pb_0" style="padding-top:0px;padding-bottom:0px; " data-record-type="581"><!-- cover --><div class="t-cover" id="recorddiv60127381" bgimgfield="img" style="height:500px; background-image:url(&#39;https://static.tildacdn.com/tild3561-3933-4838-a164-626331383138/-/resize/20x/DSC05783.jpg&#39;);"><div class="t-cover__carrier loaded" id="coverCarry60127381" data-content-cover-id="60127381" data-content-cover-bg="https://static.tildacdn.com/tild3561-3933-4838-a164-626331383138/DSC05783.jpg" data-content-cover-height="500px" data-content-cover-parallax="" style="height: 500px; background-attachment: scroll; background-image: url(&quot;https://static.tildacdn.com/tild3561-3933-4838-a164-626331383138/DSC05783.jpg&quot;);" src=""></div> <div class="t-cover__filter" style="height:500px;background-image: -moz-linear-gradient(top, rgba(0,0,0,0.90), rgba(0,0,0,0.80));background-image: -webkit-linear-gradient(top, rgba(0,0,0,0.90), rgba(0,0,0,0.80));background-image: -o-linear-gradient(top, rgba(0,0,0,0.90), rgba(0,0,0,0.80));background-image: -ms-linear-gradient(top, rgba(0,0,0,0.90), rgba(0,0,0,0.80));background-image: linear-gradient(top, rgba(0,0,0,0.90), rgba(0,0,0,0.80));filter: progid:DXImageTransform.Microsoft.gradient(startColorStr=&#39;#19000000&#39;, endColorstr=&#39;#33000000&#39;);"></div><div class="t-container"> <div class="t-col t-col_10 t-prefix_1 t-align_center"><div class="t-cover__wrapper t-valign_middle" style="height:500px; position: relative;z-index:1;"> <div class="t581"> <div data-hook-content="covercontent"> <div class="t581__wrapper t-align_center"> <div class="t581__title t-title t-title_sm t-margin_auto" style="" field="title">Хотите стать одним из нас?</div> <div class="t581__descr t-descr t-descr_xl t-margin_auto" style="max-width:600px;" field="descr">Тогда ознакомьтесь со списком наших вакансий и оставьте заявку! <br>Стань частью команды мечты!</div> <div class="t581__buttons"> <div class="t581__buttons-wrapper t-margin_auto"> <svg class="t581__arrow-icon_mobile" style="fill:#ffe100;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 70"><path d="M30.6 47.5c-.6-.5-1.6-.4-2.1.2L19 59.6V5.5c0-.8-.7-1.5-1.5-1.5S16 4.7 16 5.5v54.1L6.5 47.7c-.6-.6-1.5-.7-2.1-.2-.7.5-.8 1.5-.3 2.1l12.2 15.2c.3.4.7.6 1.2.6s.9-.2 1.2-.6l12.2-15.2c.5-.6.4-1.6-.3-2.1z"></path></svg> <svg class="t581__arrow-icon " style="fill:#ffe100; " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 180"><path d="M54.1 108c-.5 0-.9-.2-1.2-.6-.5-.7-.3-1.6.4-2.1 1.5-1 9.5-5.5 14.6-8.3-17.4-.5-31.3-7.3-41.3-20C9.9 55.7 9.5 24.2 14.2 3.7c.2-.8 1-1.3 1.8-1.1.8.2 1.3 1 1.1 1.8-4.6 19.9-4.2 50.3 11.8 70.8 9.5 12.2 23 18.6 39.9 18.9h.3l-3.2-4c-1.4-1.7-2.7-3.3-4.1-5.1-.7-.9-1.5-1.9-2.3-2.9-.5-.6-.4-1.6.2-2.1.6-.5 1.6-.4 2.1.2 0 0 0 .1.1.1l6.4 7.9c.5.6.9 1.1 1.4 1.7 1.5 1.8 3.1 3.6 4.3 5.5 0 .1.1.1.1.2.1.2.1.3.2.5v.3c0 .2 0 .3-.1.5 0 .1-.1.1-.1.2-.1.2-.2.3-.3.4-.1.1-.2.1-.3.2 0 0-.1 0-.2.1-.9.4-16 8.6-18.2 10.1-.4 0-.7.1-1 .1z"></path></svg> <a href="http://persona-ostozhenka.ru/vacancy" target="" class="t581__btn t-btn " style="color:#ffffff;border:2px solid #bfab00;background-color:#000000;border-radius:100px; -moz-border-radius:100px; -webkit-border-radius:100px;"><table style="width:100%; height:100%;"><tbody><tr><td>Посмотреть вакансии</td></tr></tbody></table></a> </div> </div> </div> </div> </div> </div> </div> </div> </div>
   </div>

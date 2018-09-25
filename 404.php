34c5f9fa5a2aee44050de4244ce0a48466eb7f0c
usimonline <usim.online@mail.ru>
2018-09-25 07:25:36 +0300

    1

---
+++ 404.php
@@ -0,0 +1,139 @@
+<?php
+require_once ('requisites.php');
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+  <?php
+  require_once ('head.php');
+  ?>
+
+</head>
+
+
+<body class="t-body" style="margin: 0px;">
+
+<!--allrecords-->
+
+<div id="allrecords" class="t-records t-records_animated t-records_visible" data-hook="blocks-collection-content-node" data-tilda-project-id="538753" data-tilda-page-id="3190780" data-tilda-page-alias="main" data-tilda-formskey="3f508fc2ea7d23fde77f4bd2720b21b7" style="overflow-x: hidden;">
+
+
+  <?php
+  require_once ('header.php');
+  ?>
+
+
+
+
+
+  <div id="rec62126363" class="r t-rec t-rec_pt_30 t-rec_pb_45 r_showed r_anim" style="padding-top:30px;padding-bottom:45px; " data-record-type="65"><!-- T056 --><div class="t056"> <div class="t-container t-align_center"> <div class="t-col t-col_10 t-prefix_1"> <div class="t056__title t-name t-name_xl" field="title" style="">Услуги &amp; Цены</div> <div class="t056__descr t-text t-text_sm" field="descr" style="">Данные цены указаны, исходя из грейда (категории) специалиста:<br>стилист / топ-стилист</div> </div> </div></div>
+  </div>
+  <div id="rec62126353" class="r t-rec t-rec_pt_0 t-rec_pb_0 r_showed r_anim" style="padding-top:0px;padding-bottom:0px; " data-record-type="812">
+    <!-- t812 -->
+    <div class="t812"> <div class="t-container">
+        <div class="t812__col t-col t-col_6 ">
+
+          <div class="t812__pricelist-item ">
+            <div class="t812__pricelist-item__row_1 t-row">
+              <div class="t812__pricelist-item__title t-name t-name_sm" field="li_title__1516786569096" style="text-transform:uppercase;">Это страница ошибки.
+              </div>
+              <div class="t812__pricelist-item__price t-name t-name_sm" field="li_text__1516786569096" style="">404
+              </div>
+            </div>
+            <div class="t812__pricelist-item__line" style="opacity:0.9;">
+
+            </div>
+          </div>
+
+            <div class="t812__pricelist-item__line" style="opacity:0.9;">
+
+            </div>
+          </div>
+        </div>
+        <div class="t812__col t-col t-col_6 ">
+
+          <div class="t812__pricelist-item ">
+
+            <div class="t812__pricelist-item__row_1 t-row">
+              <div class="t812__pricelist-item__title t-name t-name_sm" field="li_title__1516790700047" style="text-transform:uppercase;">Введенной вами страницы не существует.</div>
+              <div class="t812__pricelist-item__price t-name t-name_sm" field="li_text__1516790700047" style="">Извините!</div> </div>
+            <div class="t812__pricelist-item__line" style="opacity:0.9;"></div> </div> <div class="t812__pricelist-item ">
+
+
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <style>#rec62126353 .t812__col:first-child {	;
+      }
+      @media screen and (max-width: 960px){ #rec62126353 .t812__col:first-child {	margin-bottom: 30px;; }
+      }
+    </style>
+  </div>
+
+  <div id="rec64327751" class="r t-rec t-rec_pt_30 t-rec_pb_30 r_showed r_anim" style="padding-top:30px;padding-bottom:30px; " data-record-type="375">
+    <!-- T375 -->
+    <div class="t375">
+      <div class="t-container">
+        <div class="t-col t-col_12 t-align_center">
+          <div class="t375__img"> <svg width="50px" height="11.93px" viewBox="0 0 88 21"> <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g fill="#414042"> <g> <g> <path d="M77.2,20.9 C77.2,20.9 76.3,16.6 73.7,14 C71.1,11.4 66.8,10.5 66.8,10.5 C66.8,10.5 71.1,9.6 73.7,7 C76.3,4.3 77.2,0 77.2,0 C77.2,0 78.1,4.3 80.7,7 C83.3,9.6 87.7,10.5 87.7,10.5 C87.7,10.5 83.4,11.4 80.8,14 C78.1,16.6 77.2,20.9 77.2,20.9 L77.2,20.9 Z" id="Shape"></path> <path d="M44.3,20.9 C44.3,20.9 43.4,16.6 40.8,14 C38.2,11.4 33.9,10.5 33.9,10.5 C33.9,10.5 38.2,9.6 40.8,7 C43.4,4.3 44.3,0 44.3,0 C44.3,0 45.2,4.3 47.8,7 C50.4,9.6 54.8,10.5 54.8,10.5 C54.8,10.5 50.5,11.4 47.9,14 C45.2,16.6 44.3,20.9 44.3,20.9 L44.3,20.9 Z" id="Shape"></path> <path d="M11.1,20.9 C11.1,20.9 10.2,16.6 7.6,14 C5,11.4 0.7,10.5 0.7,10.5 C0.7,10.5 5,9.6 7.6,7 C10.1,4.3 11.1,0 11.1,0 C11.1,0 12,4.3 14.6,7 C17.2,9.6 21.6,10.5 21.6,10.5 C21.6,10.5 17.3,11.4 14.7,14 C12,16.6 11.1,20.9 11.1,20.9 L11.1,20.9 Z" id="Shape"></path> </g> </g> </g> </g> </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div id="rec62126650" class="r t-rec t-rec_pt_60 t-rec_pb_45 r_anim r_showed" style="padding-top:60px;padding-bottom:45px; " data-record-type="210"><!-- T186 --><div class="t186"> <form id="form62126650" name="form62126650" role="form" action="https://forms.tildacdn.com/procces/" method="POST" data-formactiontype="2" data-inputbox=".t186__blockinput" class="js-form-proccess " data-tilda-captchakey=""> <input type="hidden" name="formservices[]" value="f32db6c20d27441ba18bd353de2c771a" class="js-formaction-services"> <input type="hidden" name="formservices[]" value="bcbc9b9f986b815ad82f81f2e90a0d53" class="js-formaction-services"> <input type="hidden" name="formservices[]" value="20d2c765d12e5158ffcf4c89287d0f87" class="js-formaction-services"> <input type="hidden" name="formservices[]" value="fc4024a2092c777d01e2a37032ac3b09" class="js-formaction-services"> <input type="hidden" name="formservices[]" value="f61407e08ce358956007b15f831baf49" class="js-formaction-services"> <div class="t-container"> <div class="t-col t-col_8 t-prefix_2"> <div> <div class="js-errorbox-all t186__blockinput-errorbox" style="display:none;"> <div class="t186__blockinput-errors-text t-text t-text_md"> <p class="t186__blockinput-errors-item js-rule-error js-rule-error-all"></p> <p class="t186__blockinput-errors-item js-rule-error js-rule-error-req">Required field</p> <p class="t186__blockinput-errors-item js-rule-error js-rule-error-email">Please correct e-mail address</p> <p class="t186__blockinput-errors-item js-rule-error js-rule-error-name">Name Wrong. Correct please</p> <p class="t186__blockinput-errors-item js-rule-error js-rule-error-phone">Please correct phone number</p> <p class="t186__blockinput-errors-item js-rule-error js-rule-error-string">Please enter letter, number or punctuation symbols.</p> </div> </div> <div class="js-successbox t186__blockinput-success t-text t-text_md" style="display:none;"> Данные успешно отправлены. Спасибо! </div> </div> <div class="t186__wrapper"> <div class="t186__blockinput"> <input type="text" name="tel" class="t186__input t-input js-tilda-rule " value="" placeholder="Ваш № телефона" data-tilda-rule="phone" style="color:#000000; border:1px solid #000000; "> </div> <div class="t186__blockbutton"> <button type="submit" class="t-submit" style="color:#ffffff;background-color:#000000;">Записаться со скидкой! *</button> </div> </div> </div> </div> <div style="position: absolute; left: -5000px; bottom:0;"><input type="text" name="form-spec-comments" value="Its good" class="js-form-spec-comments" tabindex="-1"></div></form>
+    </div><style>#rec62126650 input::-webkit-input-placeholder {color:#000000; opacity: 0.5;}
+      #rec62126650 input::-moz-placeholder {color:#000000; opacity: 0.5;}
+      #rec62126650 input:-moz-placeholder {color:#000000; opacity: 0.5;}
+      #rec62126650 input:-ms-input-placeholder {color:#000000; opacity: 0.5;}
+    </style>
+  </div>
+  <div id="rec62126695" class="r t-rec t-rec_pt_0 t-rec_pb_15" style="padding-top:0px;padding-bottom:15px;background-color:#000000; " data-record-type="56" data-bg-color="#000000" data-animationappear="off">
+    <!-- T046 -->
+    <div class="t046">
+      <div class="t-container">
+        <div class="t-col t-col_6 t-prefix_3">
+          <div class="t046__text t-text t-text_xs" style="" field="text"><div style="font-size:22px;color:#ffffff;" data-customstyle="yes">
+              <span style="font-weight: 700;">*-5% на все услуги имидж-студии<br>Скидка действует только при онлайн-обращении.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+  <?php
+  require_once ('footer.php');
+  ?>
+
+</div>
+
+<div class="t360__progress" style="display: none;">
+  <div class="t360__bar t360__barprogressfinished t360__barprogresshidden">
+
+  </div>
+</div>
+
+<script type="text/javascript" src="./index/json.txt">
+
+</script>
+
+</body>
+</html>

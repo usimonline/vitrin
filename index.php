f4a367b7623f9812fec0915ccaa0232db04c6fd8
usimonline <usim.online@mail.ru>
2018-09-29 11:48:11 +0300

    1

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 set_time_limit(0);
-//require_once("functions.php"); //бесполезная часть
+require_once("functions.php"); //бесполезная часть
 require_once("requisites.php");
 require_once("base.php");//просто база данных
 require_once("route.php");
-//if (!$route) require_once("login_users.php");
+if (!$route) require_once("login_users.php");
 
 
 ?>
